d70c9c721407dacc6925709d816cbde76ab28e7e
Nathaniel Stevens <nathaniel.scott.stevens@gmail.com>
2018-11-25 16:50:03 -0500

    Adds documentation and changes creates function signature.

--- lib/grid.ex
+++ lib/grid.ex
@@ -1,5 +1,27 @@
 defmodule Grid do
-  def create({width, height}, default \\ nil) do
+  @moduledoc """
+  Grid provides easy generation and manipulation
+  of two dimensional lists.
+  """
+
+  @doc """
+  Generate a new grid.
+
+  Returns a two dimensional list
+
+  If width or height are negative,
+  the grid will return with a corresponding
+  width or height of 0
+
+  ## Examples
+
+      iex> Grid.create(2, 2)
+      [[nil, nil], [nil, nil]]
+
+      iex> Grid.create(2, 2, 5)
+      [[5, 5], [5, 5]]
+  """ 
+  def create(width, height, default \\ nil) do
     width = if (width < 0), do: 0, else: width
     height = if (height < 0), do: 0, else: height
 
@@ -7,6 +29,19 @@ defmodule Grid do
     |> List.duplicate(height)
   end
 
+  @doc """
+  Get grid value at position.
+
+  * The origin of the grid is the bottom left corner.
+  * Negative positions will wrap backwards.
+  * If position is out of bounds, returns `nil`.
+
+  ## Examples
+
+    iex> Grid.at([[1, 2], [3, 4]], {1, 1})
+    2 
+  """
+  def at(grid, position)
   def at(grid, {x, y}) do
     y_inverted = Enum.count(grid) - 1 - y
 
--- test/grid_test.exs
+++ test/grid_test.exs
@@ -4,12 +4,12 @@ defmodule GridTest do
 
   describe "create" do
     test "makes correct number of rows" do
-      subject = Grid.create({2, 3}) 
+      subject = Grid.create(2, 3) 
       assert Enum.count(subject) == 3
     end
 
     test "makes correct number of columns" do
-      subject = Grid.create({2, 3}) 
+      subject = Grid.create(2, 3) 
       actual = Enum.at(subject, 0)
       |> Enum.count() 
 
@@ -17,16 +17,16 @@ defmodule GridTest do
     end
 
     test "sets correct default value" do
-      subject = Grid.create({2, 3}, 1) 
+      subject = Grid.create(2, 3, 1) 
       assert subject |> Enum.all?()
     end
 
     test "returns an empty list when rows are less than 1" do
-      assert [] == Grid.create({5, -1}) 
+      assert [] == Grid.create(5, -1) 
     end
 
     test "returns an list with empty columns when columns are less than 1" do
-      assert [[], []] == Grid.create({-1, 2}) 
+      assert [[], []] == Grid.create(-1, 2) 
     end
   end
 
